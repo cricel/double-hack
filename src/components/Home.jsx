ce14dde319e170904b82d44aab0e9dcb1a0b6589
Xiangxu Lin <cricel.design@gmail.com>
2021-07-07 00:03:09 -0400

    base 2d nav with button,,,pose & goal visulize is not working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,42 @@
-import React, { Component } from 'react';
-import { Button } from "react-bootstrap";
+import React, { Component } from "react";
+import Connection from "./Connection";
+import Teleoperation from "./Teleoperation";
+import RobotState from "./RobotState";
+import Map from "./Map";
+import { Row, Col, Container, Button } from "react-bootstrap";
 
 class Home extends Component {
-    state = {
-        counter: 0,
-    }
+  state = {};
 
-    increment_counter(){
-        this.setState({counter: this.state.counter + 1});
-    }
+  render() {
+    return (
+      <div>
+        <Container>
+          <h1 className="text-center"> Home</h1>
+          <Row>
+            <Col>
+              <Connection />
+            </Col>
+          </Row>
 
-    render() {
-        return (
-            <div>
-                <h1 className="text-center"> Home</h1>
-                <h5>
-                    Counter: 
-                    <span>{this.state.counter}</span>
-                </h5>
-                <Button onClick={() => this.increment_counter()}>Increment</Button>
-            </div>
-        );
-    }
+          <Row>
+            <Col>
+              <Teleoperation />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <RobotState />
+            </Col>
+            <Col>
+              <h1>Map</h1>
+              <Map />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Home;
